8c7f3dff1511349d4a3807b88257adfbe40d60c8
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-04-22 19:15:34 +0200

    [add Override on method]

--- src/main/java/fr/esgi/security/SecurityConfig.java
+++ src/main/java/fr/esgi/security/SecurityConfig.java
@@ -104,6 +104,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
