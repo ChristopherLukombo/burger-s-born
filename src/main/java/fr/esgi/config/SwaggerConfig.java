11f2e0a91b8b0405dc31650008d1b56887111803
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-05-22 12:40:53 +0200

    [add config for deployment]

--- src/main/java/fr/esgi/config/SwaggerConfig.java
+++ src/main/java/fr/esgi/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package fr.esgi.config;
 
-import fr.esgi.security.jwt.TokenProvider;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+
+import fr.esgi.security.jwt.TokenProvider;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -16,10 +20,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
 
-@Profile("dev")
 @Configuration
 @EnableSwagger2
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -32,6 +33,7 @@ public class SwaggerConfig {
         this.tokenProvider = tokenProvider;
     }
 
+    @Profile("dev")
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
